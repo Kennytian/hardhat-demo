1a42ff692f0448625ab23d67b616d40e045c88fa
Kennytian <kt.tian@gmail.com>
2022-05-19 21:03:08 +0800

    feat: Update code

---
+++ contracts/GlobalVariables.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract GlobalVariables {
+    // view 会读取一些变量的值
+    // 状态变量、全局变量，都可以用 view 来读
+    // pure 是不能读取状态变量、全局变量
+    function globalVars() external view returns (address, uint, uint) {
+        address sender = msg.sender;
+        uint timestamp = block.timestamp;
+        uint blockNum = block.number;
+        return (sender, timestamp, blockNum);
+    }
+}
---
+++ contracts/StateVariables.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract StateVariable {
+    uint public myUnit = 123;
+
+    // external 外部可用
+    function foo() external {
+        uint noStateVariable = 456;
+    }
+}
