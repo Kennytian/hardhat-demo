3ef721cc0ccea93ef7066b1e522f407ee82b2c69
Kennytian <kt.tian@gmail.com>
2022-05-20 11:26:47 +0800

    feat: Update code

---
+++ contracts/Counter.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Counter {
+    // 只有外部才能读取，所以不需要 view 或 pure 关键字
+    uint public count = 0;
+
+    //因为是写入方法，所以不需要 view 或 pure 关键字
+    function inc() external {
+        count += 1;
+    }
+
+    // 可视范围是外部可视，合约的内部的其它函数是不能调用的，只能通过外部读取
+    function dec() external {
+        count -= 1;
+    }
+}
---
+++ contracts/DefaultVaules.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract DefaultValues {
+    bool public b = false;
+    uint public u = 0;
+    int public i = 0;
+    address public a;  // 0x 后面40个0
+    bytes32 public b32; //0x 后面64个0
+    // mapping, structs, enums, fixed sized arrays
+}
