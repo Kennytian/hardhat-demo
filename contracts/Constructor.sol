cea56506c663d60a5a60e4a7755981e0e19ec31a
Kennytian <kt.tian@gmail.com>
2022-05-21 20:52:17 +0800

    feat: Update code

---
+++ contracts/Constructor.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Constructor {
+    address public owner;
+    uint public x;
+
+    // 用结构函数初始化变量的操作
+    constructor(uint _x){
+        owner = msg.sender;
+        x = _x;
+    }
+}
