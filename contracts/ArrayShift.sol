e3bbf694c1d14a4265273f889285584e1d2e91ec
Kennytian <kt.tian@gmail.com>
2022-05-23 16:14:07 +0800

    feat: Update code

--- contracts/ArrayShift.sol
+++ contracts/ArrayShift.sol
@@ -15,16 +15,16 @@ contract ArrayShift {
     // [1, 2, 3, 4, 5, 6] -> remove(2) -> [1, 2, 4, 5, 6, 6] -> [1, 2, 4, 5, 6]
     // [1] -> remove(0) -> [1] -> []
     function remove(uint _index) public {
-        require(_index < arr.length, 'index out of bound');
+        require(_index < arr.length, "index out of bound");
         for (uint i = _index; i < arr.length - 1; i++) {
-            arr[i] = arr[i+1];
+            arr[i] = arr[i + 1];
         }
         arr.pop();
     }
 
 
     function test() external {
-        arr = [1, 2, 3 ,4, 5];
+        arr = [1, 2, 3, 4, 5];
         remove(2);
         // [1, 2, 4, 5];
         assert(arr[0] == 1);
